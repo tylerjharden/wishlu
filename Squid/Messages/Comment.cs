398d763f1b192925c51a67a04bdc857d78ed975b
tylerjharden <tyler@wishlu.com>
2015-03-17 12:00:00 -0400

    New Commenter class to store details on the entity that posts a comment

--- Squid/Messages/Comment.cs
+++ Squid/Messages/Comment.cs
@@ -8,21 +8,93 @@ using System.Linq;
 
 namespace Squid.Messages
 {
+    public enum CommentType
+    {
+        comment,
+        image,
+        wish,
+        link
+    }
+
     public class Comment : SocialGraphObject
     {
-        [JsonProperty("AuthorId")]
-        public Guid AuthorId { get; set; }
+        [JsonProperty("commenter")]
+        public Commenter Commenter { get; set; }
 
-        [JsonProperty("Body")]
-        public String Body { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("deletable_by_me")]
+        public bool DeletableByMe
+        {
+            get
+            {
+                return this.Commenter.Id == User.CurrentUserId;
+            }
+        }
 
         public Comment()
         {
             Id = Guid.Empty;
-            AuthorId = Guid.Empty;
-            Body = String.Empty;
-            
-            Logger.Log("new Comment()");
-        }        
+            Commenter = new Messages.Commenter();
+            Text = String.Empty;            
+        }       
+ 
+        public Comment(Guid userId, string text)
+        {
+            Id = Guid.NewGuid();
+            Commenter = new Commenter(userId);
+            Text = text;
+        }
+    }
+
+    public enum CommenterType
+    {
+        user,
+        store,
+        wishlu
+    }
+
+    public class Commenter
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("full_name")]
+        public string FullName { get; set; }
+
+        [JsonProperty("type")]
+        public CommenterType Type { get; set; }
+
+        [JsonProperty("image_url")]
+        public string ImageUrl { get; set; }
+
+        public Commenter()
+        {
+            Id = Guid.Empty;
+            Username = string.Empty;
+            FullName = string.Empty;
+            Type = CommenterType.user;
+            ImageUrl = string.Empty;
+        }
+
+        public Commenter(Guid userId)
+        {
+            var user = User.GetUserById(userId);
+
+            Id = user.Id;
+            Username = user.Handle;
+            FullName = user.FullName;
+            Type = CommenterType.user;
+            ImageUrl = user.Image;
+        }
+
+        public Commenter(User user)
+        {
+
+        }
     }
 }
